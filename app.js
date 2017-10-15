db9dd268e6c284aa7561f5899a4f6f0b58a5892b
kautukkundan <kautukkundan@gmail.com>
2017-10-16 02:31:54 +0530

    file uploader added

--- app.js
+++ app.js
@@ -2,24 +2,37 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var fs = require("fs");
 var app = express();
+var upload = require('express-fileupload');
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+app.use(upload());
 
 app.set("view engine", "ejs");
 
 app.use(express.static("public"));
 
-
 app.get("/home", function (req, res) {
-  res.render("home");
+	res.render("home");
 });
 
-app.get("/login", function (req, res) {
-  res.render("login");
-});
+app.post("/home", function(req, res){
+	if (!req.files)
+		return res.status(400).send('No files were uploaded.');
 
-app.post("/home",urlencodedParser, function (req, res) {
-  console.log(req.body);  
+  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+  let sampleFile = req.files.sampleFile;
+  var filename = sampleFile.name;
+  
+
+  // Use the mv() method to place the file somewhere on your server
+  sampleFile.mv('./uploads/'+filename, function(err) {
+  	if (err){
+  		return res.status(500).send(err); 
+  		res.send("Some Error Uploading the File...Please Try Again")
+  	}
+  	res.redirect('home');
+  });
 });
 
 app.listen(3000, "0.0.0.0");
